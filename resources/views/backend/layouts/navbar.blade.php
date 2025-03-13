ac268c05de9e2bf7b73b0889c79fa96a1ab8b7cf
Md Rakibul Hasan <73007569+rakibulhasan65@users.noreply.github.com>
2025-03-13 14:09:25 +0600

    Update navbar.blade.php

--- resources/views/backend/layouts/navbar.blade.php
+++ resources/views/backend/layouts/navbar.blade.php
@@ -43,7 +43,7 @@
         </li>
 
         <!-- Messages Dropdown Menu -->
-        <li class="nav-item dropdown">
+        {{-- <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-comments"></i>
                 <span class="badge badge-danger navbar-badge">3</span>
@@ -102,9 +102,9 @@
                 <div class="dropdown-divider"></div>
                 <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
             </div>
-        </li>
+        </li> --}}
         <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
+        {{-- <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-bell"></i>
                 <span class="badge badge-warning navbar-badge">15</span>
@@ -134,7 +134,7 @@
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
-        </li>
+        </li> --}}
 
 
         <li class="nav-item">
