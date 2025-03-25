be59cb15172b2688a3b1a7f2f50d711c6dd1b74d
Md Rakibul Hasan <73007569+rakibulhasan65@users.noreply.github.com>
2025-03-25 23:00:21 +0600

    Development

--- resources/views/frontend/pages/evisa_inquiry_form.blade.php
+++ resources/views/frontend/pages/evisa_inquiry_form.blade.php
@@ -23,7 +23,7 @@
         }
 
         .btn-submit {
-            background-color: #2f79df;
+            background-color: #0060c7;
             color: white;
         }
     </style>
@@ -32,10 +32,9 @@
 <body class="bg-gray-100 flex justify-center items-center min-h-screen">
 
     <div class="card w-full h-screen max-w-md mx-auto min-h-[450px] bg-white shadow-lg">
-
         <!-- Header with Back Icon -->
-        <div class="bg-[#35609c] flex items-center p-3 ">
-            <button class="mr-4"  onclick="window.history.back();">
+        <div class="bg-[#1d305b] flex items-center p-3 ">
+            <button class="mr-4" onclick="window.history.back();">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" viewBox="0 0 24 24"
                     stroke="currentColor" fill="none" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7"></path>
@@ -46,31 +45,31 @@
 
         <!-- Form Section -->
         <div class="p-6">
-            <p class="text-gray-600 text-sm mb-4">Fill the following information to retrieve the visa details</p>
+            <p class="text-blue-700 text-sm mb-4 font-bold">Fill the following information to retrieve the visa details</p>
 
             <form action="{{ route('web-app-evisa-details') }}" method="POST">
                 @csrf
                 <div class="space-y-4">
                     <div>
-                        <label for="visa_number" class="text-xs text-gray-500">Visa Number</label>
+                        <label for="visa_number" class="text-sm text-black-500">Visa Number</label>
                         <input type="text" name="visa_number" id="visa_number"
-                            class="w-full p-2 border border-gray-300 rounded-lg" required>
+                            class="w-full p-3 border border-gray-300 rounded-lg" placeholder="Enter your visa number" required>
                     </div>
 
                     <div>
-                        <label for="mio_reference" class="text-xs text-gray-500">MIO Reference</label>
+                        <label for="mio_reference" class="text-sm text-black-500">MIO Reference</label>
                         <input type="text" name="mio_reference" id="mio_reference"
-                            class="w-full p-2 border border-gray-300 rounded-lg" required>
+                            class="w-full p-3 border border-gray-300 rounded-lg" placeholder="Enter your MOI reference" required>
                     </div>
 
                     <div>
-                        <label for="passport_number" class="text-xs text-gray-500">Passport Number</label>
+                        <label for="passport_number" class="text-sm text-black-500">Passport Number</label>
                         <input type="text" name="passport_number" id="passport_number"
-                            class="w-full p-2 border border-gray-300 rounded-lg" required>
+                            class="w-full p-3 border border-gray-300 rounded-lg" placeholder="Enter your passport number" required>
                     </div>
 
                     <div>
-                        <button type="submit" class="w-full py-2 rounded-lg btn-submit">Submit</button>
+                        <button type="submit" class="w-full py-3 rounded-lg btn-submit">Inquiry</button>
                     </div>
                 </div>
             </form>
